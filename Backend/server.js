6bcb1593f39ea582267037c77eccfb7fc79163fe
Vyoobam-Tech <info@vyoobam.com>
2026-01-28 14:22:52 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -28,8 +28,8 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
-mongoose.connect("mongodb://127.0.0.1:27017/inventory")
-// mongoose.connect(process.env.MONGO_URI) 
+//mongoose.connect("mongodb://127.0.0.1:27017/inventory")
+ mongoose.connect(process.env.MONGO_URI) 
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
 app.use("/api/products", productRoutes);
